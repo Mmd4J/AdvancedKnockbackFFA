112e7dabc99d01c61996eb31a6eb94c9c35f256b
Soheil <TheWylot22@gmail.com>
2022-06-12 21:14:09 +0430

    CLEANUP // Base x3

--- src/main/java/me/gameisntover/knockbackffa/gui/LightButton.java
+++ src/main/java/me/gameisntover/knockbackffa/gui/LightButton.java
@@ -5,12 +5,15 @@ import lombok.Setter;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
-@Getter @Setter
+@Getter
+@Setter
 public abstract class LightButton {
     private ItemStack item;
-    public LightButton(ItemStack item){
+
+    public LightButton(ItemStack item) {
         this.item = item;
 
     }
+
     public abstract void onClick(InventoryClickEvent e);
 }
--- src/main/java/me/gameisntover/knockbackffa/gui/LightGUI.java
+++ src/main/java/me/gameisntover/knockbackffa/gui/LightGUI.java
@@ -25,17 +25,20 @@ public class LightGUI implements Listener {
     private Consumer<InventoryOpenEvent> openEvent;
     private Consumer<InventoryCloseEvent> closeEvent;
     private boolean destroyOnClose = false;
+
     public LightGUI(String name, Integer slot) {
         inventory = Bukkit.createInventory(null, slot * 9, ChatColor.translateAlternateColorCodes('&', name));
         Bukkit.getPluginManager().registerEvents(this, KnockbackFFALegacy.getInstance());
     }
-    public void setOnDestroyClose(Boolean toggle){
-    destroyOnClose = toggle;
+
+    public void setOnDestroyClose(Boolean toggle) {
+        destroyOnClose = toggle;
     }
+
     public void addButton(LightButton button) {
         for (int i = 0; i < inventory.getMaxStackSize(); i++) {
             if (!guiButtonMap.containsKey(i)) {
-                setButton(button,i);
+                setButton(button, i);
                 break;
             }
         }
@@ -45,24 +48,28 @@ public class LightGUI implements Listener {
         guiButtonMap.put(i, button);
         inventory.setItem(i, button.getItem());
     }
+
     public void setButton(LightButton button, int x, int y) {
-        setButton(button,x + y * 9 - 1);
+        setButton(button, x + y * 9 - 1);
     }
+
     @EventHandler
     public void onInventoryClickEvent(InventoryClickEvent e) {
         if (e.getInventory() != inventory) return;
         LightButton button = guiButtonMap.get(e.getSlot());
         button.onClick(e);
     }
+
     @EventHandler
-    public void onInventoryOpen(InventoryOpenEvent e){
+    public void onInventoryOpen(InventoryOpenEvent e) {
         if (e.getInventory() == inventory && openEvent != null) openEvent.accept(e);
     }
+
     @EventHandler
-    public void onInventoryClose(InventoryCloseEvent e){
-        if (e.getInventory() == inventory ){
+    public void onInventoryClose(InventoryCloseEvent e) {
+        if (e.getInventory() == inventory) {
             if (closeEvent != null)
-            closeEvent.accept(e);
+                closeEvent.accept(e);
             if (destroyOnClose) HandlerList.unregisterAll(this);
         }
     }
