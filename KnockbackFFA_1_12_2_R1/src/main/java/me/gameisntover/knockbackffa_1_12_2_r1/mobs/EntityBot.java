cb808e67030926d2dfa3a863dca9cb2b56c340f0
MmdGIO <hosun1451@gmail.com>
2022-08-12 09:30:44 +0430

    boo

---
+++ KnockbackFFA_1_12_2_R1/src/main/java/me/gameisntover/knockbackffa_1_12_2_r1/mobs/EntityBot.java
@@ -0,0 +1,4 @@
+package me.gameisntover.knockbackffa_1_12_2_r1.mobs;
+
+public class EntityBot {
+}
---
+++ KnockbackFFA_1_8_R3/src/main/java/me/gameisntover/knockbackffa_1_8_r3/mobs/EntityBot.java
@@ -0,0 +1,14 @@
+package me.gameisntover.knockbackffa_1_8_r3.mobs;
+
+import net.minecraft.server.v1_8_R3.EntityZombie;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+
+public class EntityBot extends EntityZombie {
+    public EntityBot(Location loc,String name) {
+        super(((CraftWorld)loc.getWorld()).getHandle());
+        setCustomName(name);
+        setCustomNameVisible(true);
+    }
+
+}
